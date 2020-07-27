7147aaf55bb127a0c4dba043835db317d3893372
khomsark <komsark_@hotmail.co.th>
2020-07-27 20:55:37 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import React,{useState} from 'react';
+import React from 'react';
 import firestore from './firebase/firebase'
+// const App = ()=>{
+//     const ref = firestore.collection('users').doc("DPyOrXTzvXRLkhUZZNrT")
+//     ref.get()
+//     .then((doc)=>{
+//       if(doc.exists){
+//       console.log(`id: ${doc.id}`)
+//       console.log(`Document Data : ${doc.data()}`)
+//       console.log(`Field userName : ${doc.get("username")}`)
+//     }
+//     else console.log("No such document")
+//   }).catch((err)=>console.log(err))
+//   return <div></div>
+//   }
+  
 const App = ()=>{
-  const [username,setUserName] = useState("")
-  const [age,setAge] = useState("")
-  const addUserHandler = (obj)=>{
-    const ref = firestore.collection('users')
-    const id = "myid#"+Math.random(999).toString()
-    ref
-    .doc(id)
-    .set(obj)
-    .then(()=>{
-      console.log("add success full")
-    }).catch((err)=>console.log(err))
-  }
-  const submitHandler = (e)=>{
-    e.preventDefault()
-    const obj = {
-    username : username,
-      age : age,
-    }
-    setUserName("")
-    setAge("")
-    addUserHandler(obj)
-  }
-  return (
-    <div>
-        <div style={{width:"80%",marginLeft:20}}>
-          <form onSubmit={submitHandler}>
-            <label>ชื่อ</label>
-            <input type="text" value={username} onChange={(e)=> setUserName(e.target.value)}></input>
-            <label>อายุ</label>
-            <input type="text" value={age} onChange={(e)=> setAge(e.target.value)}></input>
-            <button type="submit">Add</button>
-            </form>
-           </div>
-          </div>  
-  )
+  const ref = firestore.collection("users").doc("DPyOrXTzvXRLkhUZZNrT")
+ const a =  ref.update({username:"kuykuy"})
+  console.log(a)
+  
+  return <div></div>
 }
+
 export default App;
